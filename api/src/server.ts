8ba2acc524a3c6b987f03541a8241ff41f9c1108
Peterson Adami Candido <contact.petersonac@gmail.com>
2021-02-23 21:16:35 -0300

    Day 2 :clap:

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,26 +1,11 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database'
+import { router } from './routes'
 
 const app = express()
 
-/**
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração especifica
- */
-
-// 1 param => Rota
-// 2 param => request, response
-
-// GET http://localhost:3333/
-app.get("/", (request, response) => {
-    return response.json("Hello word! - NLW04")
-})
-
-app.post("/", (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso!"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log("Running!"))
